554c93ae29a2aac2b92de52443cd651d54b5b7f7
AppleDannyClegg <jives.gimbals0@icloud.com>
2025-01-08 10:21:39 +0000

    added wip to sourc control

---
+++ src/public/js/elements.js
@@ -0,0 +1,64 @@
+class CyclingHeader extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+       <nav
+      class="navbar navbar-expand-lg bg-dark bg-body-tertiary fixed-top"
+      data-bs-theme="dark"
+    >
+      <div class="container-fluid">
+        <a class="navbar-brand" href="./index.html">Cycling</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="./index.html"
+                >Home</a
+              >
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="./road.html">Road</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="./track.html">Track</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./cyclocross.html">Cyclocross</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="./mountainbike.html">Mountain Bike</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="./about.html">About</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="./gallery.html">Gallery</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    `;
+  }
+}
+
+customElements.define("nav-component", CyclingHeader);
